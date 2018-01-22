6ec8222212743bf0a5503bf9e7c9e43734bc8e21
Tanner Nelson <me@tanner.xyz>
2018-01-21 20:01:48 -0500

    remove hacks file

--- Sources/Pokedex/Controllers/PokemonController.swift
+++ Sources/Pokedex/Controllers/PokemonController.swift
@@ -1,6 +1,7 @@
 import FluentSQLite
 import Vapor
 
+/// Controllers querying and storing new Pokedex entries.
 final class PokemonController {
     /// Lists all known pokemon in our pokedex.
     func index(_ req: Request) throws -> Future<[Pokemon]> {
@@ -10,25 +11,25 @@ final class PokemonController {
     /// Stores a newly discovered pokemon in our pokedex.
     func create(_ req: Request) throws -> Future<Pokemon> {
         /// Decode a Pokemon from the HTTP request body
-        return try req.content.decode(Pokemon.self).flatMap { pokemon in
+        return try req.content.decode(Pokemon.self).flatMap { newPokemon in
             /// Check to see if the pokemon already exists
-            return Pokemon.query(on: req).filter(\Pokemon.name == pokemon.name).count().flatMap(to: Bool.self) { count in
+            return Pokemon.query(on: req).filter(\.name == newPokemon.name).count().flatMap(to: Bool.self) { count in
                 /// Ensure number of Pokemon with the same name is zero
                 guard count == 0 else {
-                    throw Abort(.badRequest, reason: "You already caught \(pokemon.name).")
+                    throw Abort(.badRequest, reason: "You already caught \(newPokemon.name).")
                 }
 
                 /// Check if the pokemon is real. This will throw an error aborting
                 /// the request if the pokemon is not real.
-                return try req.make(PokeAPI.self).verifyName(pokemon.name)
+                return try req.make(PokeAPI.self).verifyName(newPokemon.name)
             }.flatMap(to: Pokemon.self) { nameVerified in
                 /// Ensure the name verification returned true, or throw an error
                 guard nameVerified else {
-                    throw Abort(.badRequest, reason: "Invalid Pokemon name.")
+                    throw Abort(.badRequest, reason: "Invalid Pokemon \(newPokemon.name).")
                 }
 
-                /// save the pokemon
-                return pokemon.save(on: req)
+                /// Save the new Pokemon
+                return newPokemon.save(on: req)
             }
         }
     }
